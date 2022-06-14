23542622f1bd9c7699230b1aaeebff708dac095e
LuisVergaraCastro <48942067+LuisVergaraCastro@users.noreply.github.com>
2022-06-14 13:19:25 +0200

    Update BOLETIN_9.1.sql

--- BOLETIN_9.1.sql
+++ BOLETIN_9.1.sql
@@ -31,15 +31,52 @@ WHERE EmployeeID NOT IN (SELECT DISTINCT E.EmployeeID
 							ON O.CustomerID = C.CustomerID
 							WHERE C.Country LIKE 'Portugal')
 --7.Total de ventas en US$ de productos de cada categoría (nombre de la categoría).
-
+SELECT C.CategoryName, SUM(OD.UnitPrice * OD.Quantity) AS [TOTAL DE VENTAS EN US$]
+FROM Categories AS C INNER JOIN Products AS P ON C.CategoryID = P.CategoryID
+					 INNER JOIN [Order Details] AS OD ON P.ProductID = OD.ProductID
+GROUP BY C.CategoryName
 --8.Total de ventas en US$ de cada empleado cada año (nombre, apellidos, dirección).
-
+SELECT E.FirstName, E.LastName, E.Address, SUM(OD.UnitPrice * OD.Quantity)AS [TOTAL DE VENTAS EN US$], YEAR(O.OrderDate) AS YEAR  FROM Employees AS E INNER JOIN Orders AS O ON E.EmployeeID = O.EmployeeID INNER JOIN [Order Details] AS OD ON O.OrderID = OD.OrderID GROUP BY E.FirstName, E.LastName, E.Address, YEAR(O.OrderDate)
 --9.Ventas de cada producto en el año 97. Nombre del producto y unidades.
-
+SELECT * FROM Products
+SELECT P.ProductID, P.ProductName, P.QuantityPerUnit, SUM(OD.UnitPrice * OD.Quantity)AS [VENTAS], YEAR(O.OrderDate) AS YEAR
+FROM Orders AS O INNER JOIN [Order Details] AS OD ON O.OrderID = OD.OrderID
+				 INNER JOIN Products AS P ON OD.ProductID = P.ProductID
+WHERE YEAR(O.OrderDate) = 1997
+GROUP BY  P.ProductID, P.ProductName, P.QuantityPerUnit, YEAR(O.OrderDate)
 --10.Cuál es el producto del que hemos vendido más unidades en cada país. *
+CREATE OR ALTER TABLE [VENTAS POR PAIS] AS
+SELECT P.ProductName, C.City
+FROM Products AS P INNER JOIN [Order Details] AS OD ON P.ProductID = OD.ProductID
+				   INNER JOIN Orders AS O ON OD.OrderID = O.OrderID
+				   INNER JOIN Customers AS C ON O.CustomerID = C.CustomerID
+GROUP BY P.ProductName, C.City
 
+SELECT [VentasTotales] AS Maximo
+--En esta consulta utilizaré la vista anterior para elegir el máximo de ventas por pais 
+Select [Ventas totales] as Maximo,
+		VA.ProductName,
+		--Como en 'Max' solo está una vez cada pais
+		--Esto nos asegurará el funcionamiento correcto de la consulta
+		Max.Country
+		--Aquí llamo a la vista creada anteriormente 
+from [Ventas por pais] as VA
+--Relaciono la vista de ventas por cada pais consigo misma,
+--pero en el segundo caso solo cojo el máximo y el país
+--ya que si no habría varios paises o productos repetidos
+inner Join (
+Select max ([ventas totales]) as Maximo
+		,Country
+from [Ventas por pais]
+group by Country) as Max
+On VA.[ventas totales] = Max.Maximo and VA.Country = Max.Country
+Order By Country
 --11.Empleados (nombre y apellidos) que trabajan a las órdenes de Andrew Fuller.
-
+Select E.FirstName, E.LastName
+From Employees as E INNER JOIN Employees as EM ON E.ReportsTo = EM.EmployeeID
+WHERE EM.FirstName = 'Andrew' AND EM.LastName = 'Fuller'
 --12.Número de subordinados que tiene cada empleado, incluyendo los que no tienen ninguno. Nombre, apellidos, ID.
-
+SELECT EM.FirstName, EM.LastName, COUNT(E.EmployeeID) AS employees
+FROM Employees AS E INNER JOIN Employees AS EM ON E.ReportsTo = EM.EmployeeID
+GROUP BY EM.FirstName, EM.LastName
 --* Se necesitan subconsultas
